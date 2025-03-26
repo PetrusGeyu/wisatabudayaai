7246dc9c47f349f23c1ad910f8be3d3db783ae5d
PetrusGeyu <petrusgeyu34@gmail.com>
2025-03-26 19:58:18 +0700

    front-emd wisata budaya ai

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Wisata from "./pages/Wisata";
+import Budaya from "./pages/Budaya";
+import Review from "./pages/Review";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/wisata" element={<Wisata />} />
+        <Route path="/budaya" element={<Budaya />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
