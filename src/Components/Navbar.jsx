adf8a5b0764cfcded5638889e173594749d2f7a4
PetrusGeyu <petrusgeyu34@gmail.com>
2025-03-29 21:19:55 +0700

    login / register

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './navbar.css';
 import { FaUserCircle } from "react-icons/fa";
+import { Link } from 'react-router';
 
 const Navbar = () => {
   return (
     <nav>
       <div className='logo'><a href="">WisataBudaya</a></div>
       <ul className='nav-links'>
-        <li><a href="#">Beranda</a></li>
-        <li><a href="#">Wisata</a></li>
+        <li><Link to='/'>Beranda</Link></li>
+        <li><Link to={'/wisata'}>Wisata</Link></li>
         <li><a href="#">Budaya</a></li>
         <div className='profile-icon'><FaUserCircle /></div>
       </ul>
--- src/pages/Wisata.jsx
+++ src/pages/Wisata.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Wisata = () => {
-  return (
-    <div>Wisata</div>
-  )
-}
+  const [data, setData] = useState(null);
 
-export default Wisata
+  useEffect(() => {
+    axios
+      .get("https://dadb-34-91-9-190.ngrok-free.app/rekomendasi")
+      .then((response) => {
+        console.log("Response Data:", response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          // Server merespons dengan status selain 2xx
+          console.error("Server error:", error.response.status);
+          console.log("Response:", error.response.data);
+        } else if (error.request) {
+          // Request dibuat tapi tidak ada respons
+          console.error("No response received:", error.request);
+        } else {
+          // Error lainnya
+          console.error("Error:", error.message);
+        }
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Wisata</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Wisata;
